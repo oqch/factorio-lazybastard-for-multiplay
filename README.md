3180d88c4fb299ca58616690ab1d1f9913a5d533
oqch <oq2chan@gmail.com>
2022-12-31 17:52:39 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Factorio Mod
+
+This mod disables manual crafting until a rocket launch. It gives some necessary items on start, instead.
+
+This mod is experimental, not stable, not for everyone.
---
+++ control.lua
@@ -0,0 +1,54 @@
+local handler = require("event_handler")
+
+local mod = {}
+
+local function give_necessry_items(player)
+    if not player.get_main_inventory() then return end
+    player.insert{ name = "solar-panel", count = 32 }
+    player.insert{ name = "accumulator", count = 8 }
+    player.insert{ name = "substation", count = 1 }
+    player.insert{ name = "assembling-machine-1", count = 1 }
+    player.insert{ name = "small-electric-pole", count = 2 }
+end
+
+local function enable_crafting(e)
+    game.players[e.player_index].force.manual_crafting_speed_modifier = 1
+end
+
+local function disable_crafting(e)
+    game.players[e.player_index].force.manual_crafting_speed_modifier = -1
+end
+
+local function main(e)
+    if  global.rocket_lauched then return end
+    disable_crafting(e)
+    local player = game.players[e.player_index]
+    give_necessry_items(player)
+end
+
+local function player_created(e)
+    main(e)
+end
+
+local function cutscene_cancelled(e)
+    if remote.interfaces["freeplay"] then
+        main(e)
+    end
+end
+
+local function rocket_launched(event)
+    global.rocket_lauched = true
+    enable_crafting(event)
+end
+
+mod.events = {
+    [defines.events.on_player_created] = player_created,
+    [defines.events.on_cutscene_cancelled] = cutscene_cancelled,
+    [defines.events.on_rocket_launched] = rocket_launched
+}
+
+mod.on_init = function()
+    global.rocket_lauched = false
+end
+
+handler.add_lib(mod)
